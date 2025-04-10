1266ea76a8f9a435da4c4b8261d7c2240ca4cf8b
Leah Zhou <zhouxy89@yahoo.com>
2025-04-10 14:54:16 +0200

    Update DebugView.swift

--- QuantiBike/Views/DebugView.swift
+++ QuantiBike/Views/DebugView.swift
@@ -1,9 +1,7 @@
-//
 //  DebugView.swift
 //  QuantiBike
 //
-//  Created by Manuel Lehé on 08.09.22.
-//
+//  Updated to reflect new 4-FSR sensor structure
 
 import SwiftUI
 import CoreLocation
@@ -13,11 +11,11 @@ struct DebugView: View {
     @Binding var debug: Bool
     @StateObject var logManager = LogManager()
     @EnvironmentObject var logItemServer: LogItemServer
-    
+
     var timer = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect()
     var startTime = Date()
     @State var runtime = 0.0
-    
+
     var body: some View {
         HStack{
             VStack{
@@ -34,32 +32,32 @@ struct DebugView: View {
                     HStack{
                         Image(systemName: "clock")
                         Text(stringFromTime(interval: runtime)).onReceive(timer) { _ in
-                                runtime = Date().timeIntervalSinceReferenceDate - startTime.timeIntervalSinceReferenceDate
-                                let brakeData: Int = logItemServer.latestBrakeData
-                                let cadence: String = logItemServer.latestCadence
-                                let pedalDataR: Int = logItemServer.latestPedalDataR
-                                let pedalDataL: Float = logItemServer.latestPedalDataL
-                                logManager.triggerUpdate(runtime: runtime, brakeData: brakeData, cadence: cadence, pedalDataR: pedalDataR, pedalDataL: pedalDataL)
+                            runtime = Date().timeIntervalSinceReferenceDate - startTime.timeIntervalSinceReferenceDate
+                            let fsr1: Int = logItemServer.latestFSR1
+                            let fsr2: Int = logItemServer.latestFSR2
+                            let fsr3: Int = logItemServer.latestFSR3
+                            let fsr4: Int = logItemServer.latestFSR4
+                            logManager.triggerUpdate(runtime: runtime, fsr1: fsr1, fsr2: fsr2, fsr3: fsr3, fsr4: fsr4)
                         }.font(.subheadline)
                     }
                     HStack{
-                        Image(systemName: "brake")
-                        //Bike Brake Data
-                       Text("Brake: \(logItemServer.latestBrakeData)").font(.subheadline)
+                        Image(systemName: "1.circle")
+                        Text("FSR1: \(logItemServer.latestFSR1)").font(.subheadline)
                     }
                     HStack{
-                        Image(systemName: "PedalWeight")
-                        //Bike Brake Data
-                        Text("Right: \(logItemServer.latestPedalDataR), Left: \(logItemServer.latestPedalDataL)").font(.subheadline)
+                        Image(systemName: "2.circle")
+                        Text("FSR2: \(logItemServer.latestFSR2)").font(.subheadline)
                     }
                     HStack{
-                        Image(systemName: "cadence")
-                        //Bike Brake Data
-                       Text("Cadence: \(logItemServer.viewCadence)").font(.subheadline)
+                        Image(systemName: "3.circle")
+                        Text("FSR3: \(logItemServer.latestFSR3)").font(.subheadline)
+                    }
+                    HStack{
+                        Image(systemName: "4.circle")
+                        Text("FSR4: \(logItemServer.latestFSR4)").font(.subheadline)
                     }
                     HStack{
                         Image(systemName: "iphone")
-                        //iPhone Rotation Rate. Docs see CMGyroData
                         if logManager.motionManager.deviceMotion != nil{
                             Text("\(logManager.motionManager.deviceMotion!)").font(.subheadline)
                         }else{
@@ -68,7 +66,6 @@ struct DebugView: View {
                     }
                     HStack{
                         Image(systemName: "speedometer")
-                        //iPhone Rotation Rate. Docs see CMGyroData
                         if logManager.motionManager.accelerometerData != nil{
                             Text("\(logManager.motionManager.accelerometerData!)").font(.subheadline)
                         }else{
@@ -77,7 +74,6 @@ struct DebugView: View {
                     }
                     HStack{
                         Image(systemName: "safari")
-                        //iPhone Rotation Rate. Docs see CMGyroData
                         Text("Longitude: \(logManager.getLongitude()), Latitude: \(logManager.getLatitude()), Altitude: \(logManager.getAltitude())").font(.subheadline)
                     }
                 }
@@ -88,7 +84,6 @@ struct DebugView: View {
                 }).buttonStyle(.borderedProminent)
             }
         }.onAppear(perform: {
-            //Alway on Display
             preventSleep()
             logManager.setSubjectId(subjectId: subjectId)
             logManager.setMode(mode: "debug")
@@ -97,6 +92,7 @@ struct DebugView: View {
             logManager.stopUpdates()
         })
     }
+
     func logHack(val:Any,label:String?){
         if label != nil{
             var _ = print("\(label!): \(val)")
@@ -104,12 +100,14 @@ struct DebugView: View {
             var _ = print(val)
         }
     }
+
     func stringFromTime(interval: TimeInterval) -> String {
         let ms = Int(interval.truncatingRemainder(dividingBy: 1) * 1000)
         let formatter = DateComponentsFormatter()
         formatter.allowedUnits = [.hour, .minute, .second]
         return formatter.string(from: interval)! + ".\(ms)"
     }
+
     func preventSleep(){
         if(UIApplication.shared.isIdleTimerDisabled == false){
             UIApplication.shared.isIdleTimerDisabled = true
